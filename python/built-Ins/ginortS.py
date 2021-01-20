3df1e355fb40214b4c04e183001e37046d6a6f3f
vanhuu <thieuvan0405@gmail.com>
2021-01-20 23:32:18 +0700

    Add new item

--- python/built-Ins/ginortS.py
+++ python/built-Ins/ginortS.py
@@ -1,2 +1,8 @@
 # Link: https://www.hackerrank.com/challenges/ginorts/problem
 
+import string
+
+lst = input()
+sortKey = (string.ascii_letters + '1357902468').index
+a = sorted(lst,key=sortKey)
+print(*a,sep='')
---
+++ python/regex and parsing/detectFloatingPointNumber.py
@@ -0,0 +1,5 @@
+# Link: https://www.hackerrank.com/challenges/introduction-to-regex/problem
+
+import re
+
+print("\n".join(re.split(r'[.,]',input())))
