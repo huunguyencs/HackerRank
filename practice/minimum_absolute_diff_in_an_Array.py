971f3079d6bec4bfccf9b0aa09924e88577416c6
Huu Nguyen <thieuvan0405@gmail.com>
2021-02-25 10:54:00 +0700

    Rename file

---
+++ practice/minimum_absolute_diff_in_an_Array.py
@@ -0,0 +1,29 @@
+# Link: https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumAbsoluteDifference function below.
+def minimumAbsoluteDifference(arr):
+    arr = sorted(arr)
+    return min([abs(x-y) for x, y in zip(arr,arr[1:])])
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    print(result)
+
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
