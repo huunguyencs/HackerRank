ca7b4af71d07067bec1615d9882777b9ea312ee0
Huu Nguyen <thieuvan0405@gmail.com>
2021-03-14 19:57:50 +0700

    Add sorting

---
+++ interview_preparation_kit/sorting/comparator.cpp
@@ -0,0 +1,55 @@
+#include<cmath>
+#include<cstdio>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+struct Player {
+    int score;
+    string name;
+};
+
+class Checker{
+public:
+  	// complete this method
+    static int comparator(Player a, Player b)  {
+        int score_diff = a.score - b.score;
+        if (score_diff == 0)
+        {
+            int name_diff = a.name.compare(b.name);
+            return name_diff < 0 ? 1 : (name_diff > 0 ? -1 : 0);
+
+        }
+        else
+            return score_diff > 0 ? 1 : -1;
+    }
+};
+
+
+
+
+bool compare(Player a, Player b) {
+    if(Checker::comparator(a,b) == -1)
+        return false;
+    return true;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector< Player > players;
+    string name;
+    int score;
+    for(int i = 0; i < n; i++){
+        cin >> name >> score;
+        Player player;
+        player.name = name, player.score = score;
+        players.push_back(player);
+    }
+    sort(players.begin(), players.end(), compare);
+    for(int i = 0; i < players.size(); i++) {
+        cout << players[i].name << " " << players[i].score << endl;
+    }
+    return 0;
+}
